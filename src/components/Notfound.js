dc1edeacc9917cdc2c1e9f66521ecb1767c4dc2c
ibrahimerkenov <44646383+ibrahimerkenov@users.noreply.github.com>
2022-01-22 13:08:31 +0300

    Component for displaying information about incorrect movie title

---
+++ src/components/Notfound.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Notfound = () => {
+  return (
+    <div className='movie-not-found smoothAppearance'>Please enter a valid movie title</div>
+  )
+}
+
+export default Notfound;
